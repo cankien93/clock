9c3f176239abf46643d84aedb7bb5fe940e62369
cankien <cankien93@gmail.com>
2020-04-27 22:56:48 +0900

    Updates

---
+++ precache-manifest.9d3fddfa0f8903e055bf8dc37180adb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7f19ab6f1804af87f733229e9eef50",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "c55fef56e08d82e1d15b",
+    "url": "/clock/static/css/main.54d5e938.chunk.css"
+  },
+  {
+    "revision": "7fd29809d7ede9477bdb",
+    "url": "/clock/static/js/2.4fb2a707.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.4fb2a707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55fef56e08d82e1d15b",
+    "url": "/clock/static/js/main.8823b4e6.chunk.js"
+  },
+  {
+    "revision": "b2ca22d6b872553a1046",
+    "url": "/clock/static/js/runtime-main.9cb49b41.js"
+  }
+]);
