67ffa6f6730e3cfc31e2d4e949ff37f98720311b
cankien <cankien93@gmail.com>
2020-04-27 22:54:35 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      date : new Date()
+    }
+  }
+
+  componentDidMount(){
+    this.timeID = setInterval( ()=>
+      this.tick(), 1000
+    )
+  }
+  componentWillUnmount(){
+    clearInterval(this.timeID)
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    })
+  }
+  render(){
+
+      return (
+        <div className='App'>
+          {this.state.date.getHours()>=19
+            ?<p>Good evening Kien </p>
+            :<p>Hello Kien </p>}
+          <h1>{this.state.date.toLocaleTimeString('vi-VI', {hour: '2-digit', minute:'2-digit'})}</h1>
+  
+        </div>
+      )
+  }
+
 }
 
 export default App;
